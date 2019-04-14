7b3a4aa00312d8354cdfd15ec1a50c73f57e1ed7
neilyoTracer <kk_cyndi@126.com>
2019-04-14 17:24:43 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
 /* import './style.css';
 import Vscode from './vscode.png';
 import Data from './data.xml'; */
+import _ from 'lodash';
 
-function getComponent() { 
-	return import(/* webpackChunkName: "lodash" */ 'lodash')
-		.then(({ default: _ }) => { 
-			const element = document.createElement('div');
+// async function getComponent() { 
+function component() {
+	// return import(/* webpackChunkName: "lodash" */ 'lodash')
+	// 	.then(({ default: _ }) => { 
+	// 		const element = document.createElement('div');
 
-			element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	// 		element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-			return element;
-		})
-		.catch(error => 'An error occured while loading the component');
+	// 		return element;
+	// 	})
+	// 	.catch(error => 'An error occured while loading the component');
+
+	const element = document.createElement('div');
+	// const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+	const button = document.createElement('button');
+	const br = document.createElement('br');
+	button.innerHTML = 'Click me and look at the console!';
+	element.innerHTML = _.join(['Hello','webpack'], ' ');
+	element.appendChild(br);
+	element.appendChild(button);
+
+	// Note that because a network request is involved, some indication
+	// of loading would need to be shown in a production-level site/app.
+	button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+		const print = module.default;
+		print();
+	});
+	return element;
 }
 
 // document.body.appendChild(component());
-getComponent().then(component => { 
+/* getComponent().then(component => { 
 	document.body.appendChild(component);
-});
+}); */
+document.body.appendChild(component());
 
 /**
  * 这里我们需要使用 default 的原因是，从 webpack v4 开始，
